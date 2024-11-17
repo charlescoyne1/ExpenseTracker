c782a8bb095c1bf5b67d2a8a7d61cd350218d150
Charles Coyne <charles.s.coyne@gmail.com>
2024-11-17 15:45:29 -0500

    hardcoded transaction in to see transaction layout in effect

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.example.expensetracker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.expensetracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var transactions : ArrayList<Transaction>
+    private lateinit var transactionAdapter : TransactionAdapter
+    private lateinit var linearLayoutManager : LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        transactions = arrayListOf(
+            Transaction("Weekend budget", 400.00),
+            Transaction("Bananas", -4.00),
+            Transaction("Gasoline", -40.90),
+            Transaction("Breakfast", -9.99),
+            Transaction("Water bottles", -4.00),
+            Transaction("Suncream", -8.00),
+            Transaction("Car Park", -15.00)
+        )
+
+        transactionAdapter = TransactionAdapter(transactions)
+        linearLayoutManager = LinearLayoutManager(this)
+
+        binding.recyclerview.apply {
+            adapter = transactionAdapter
+            layoutManager = linearLayoutManager
         }
     }
 }
